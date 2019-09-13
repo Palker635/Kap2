9ee3e1a31cce191622050ff913531a3dc6f513cd
Isac Hansson <isac.hansson@learnet.se>
2019-09-13 11:15:10 +0200

    Uppgift1

---
+++ Uppgift1.java
@@ -0,0 +1,13 @@
+	import javax.swing.*;
+	import java.util.*;
+	public class uppgift1 {
+	public static void main (String[] args) {
+		String namn = "Isac Hansson"; 
+		String adress = "Baggböle 613"; 
+		String telefon = "076-8061616";
+		String info = (namn + "\n" + adress + "\n" + telefon);
+		
+		JOptionPane.showMessageDialog(null, info); 
+		
+		}
+	}
