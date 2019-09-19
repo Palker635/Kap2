db79e9e824bc6d71778d54c64daa123a5b3dfa68
Isac Hansson <isac.hansson@learnet.se>
2019-09-19 10:56:42 +0200

    uppgift27

---
+++ Uppgift27.java
@@ -0,0 +1,17 @@
+/*
+Uppgift 2.7
+Skapat 2019-09-19
+Dev: Isac Hansson
+*/
+import javax.swing.*;
+
+public class Uppgift27 {
+	public static void main (String[] args) {
+		char c1 = '\u00D8';
+		char c2 = '\u00F8';
+		char c3 = '\u03C0';
+		JOptionPane.showMessageDialog(null, ""+c1);
+		JOptionPane.showMessageDialog(null, ""+c2);
+		JOptionPane.showMessageDialog(null, ""+c3);
+		}
+}
